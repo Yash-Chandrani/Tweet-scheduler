e9213fcdbab5149967772cb6d36f925ab2b8176a
Yash-Chandrani <yashbeskkc@gmail.com>
2022-06-11 11:17:03 +0530

    Add flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
 class MainController<ApplicationController
     def index
+        flash.now[:notice]="Logged in successfully" #flash.now means you want to display only in the current request and not after it renders
+        flash.now[:alert]="Invalid email or password"
     end
 end
